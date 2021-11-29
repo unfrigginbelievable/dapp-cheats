c428f6374e97d3ed10094cbc3cc544c37c6b5a49
Unfreakingbelieveable.eth <unfreakingbelieveable117@gmail.com>
2021-11-29 16:35:15 -0500

    initial commit

---
+++ src/IHevm.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+interface Hevm {
+    // sets the block timestamp to x
+    function warp(uint256 x) external;
+
+    // sets the block number to x
+    function roll(uint256 x) external;
+
+    // reads the slot loc of contract c
+    function store(
+        address c,
+        bytes32 loc,
+        bytes32 val
+    ) external;
+}
---
+++ src/Tokens.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+import "./IHevm.sol";
+
+contract Tokens {
+    Hevm hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+
+    // Takes an ERC20 token address
+    function setTokens(
+        address _recipient,
+        address _token,
+        uint256 _amount
+    ) public {
+        hevm.store(
+            _token,
+            keccak256(abi.encode(_recipient, uint256(0))),
+            bytes32(_amount)
+        );
+    }
+}
