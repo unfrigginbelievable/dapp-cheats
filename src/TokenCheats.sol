4401215cb0828f12590226d88bb513bc35911af6
Unfreakingbelieveable.eth <unfreakingbelieveable117@gmail.com>
2021-11-29 16:45:00 -0500

    renamed token cheats contract

--- src/TokenCheats.sol
+++ src/TokenCheats.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./IHevm.sol";
 
-contract Tokens {
+contract TokenCheats {
     Hevm hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
     // Takes an ERC20 token address
