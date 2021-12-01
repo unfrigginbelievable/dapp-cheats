133a2a2aa35e1bdccf4a8e968c68c83b34c9ac62
Unfreakingbelieveable.eth <unfreakingbelieveable117@gmail.com>
2021-12-01 13:00:36 -0500

    That didn't work quite as I intended. Going to force users to use the OZ contracts for now

--- src/TokenCheats.sol
+++ src/TokenCheats.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "ds-test/test.sol";
-import "./IERC20.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "./IHevm.sol";
 
 contract TokenCheats is DSTest {
